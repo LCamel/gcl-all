b1ce21de9da50a3b8d4c43bceed00d89e3eb1ef3
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2026-06-30 10:09:10 +0800

    Log PATH and z3 executable resolution in debug handler

--- gcl/src/Server/Handler/GCL/Debug.hs
+++ gcl/src/Server/Handler/GCL/Debug.hs
@@ -4,6 +4,7 @@
 
 module Server.Handler.GCL.Debug where
 
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.Aeson as JSON
 import qualified Data.Text as Text
 import qualified Data.Text.Lazy as TextLazy
@@ -11,6 +12,8 @@ import qualified Data.Text.Lazy.Encoding as TextLazy
 import GHC.Generics (Generic)
 import Server.Monad (ServerM, getFileState, logTextLn, readSource)
 import qualified Server.ToClient as ToClient
+import System.Directory (findExecutable)
+import System.Environment (lookupEnv)
 
 data DebugParams = DebugParams {filePath :: FilePath}
   deriving (Eq, Show, Generic)
@@ -21,6 +24,16 @@ instance JSON.ToJSON DebugParams
 
 handler :: DebugParams -> ServerM ()
 handler DebugParams {filePath} = do
+  -- Show the PATH this server process actually received (set by the VS Code
+  -- extension, which prepends the bundled bin/ dir) and whether `z3` resolves
+  -- on it. findExecutable does the same PATH search sbv uses to locate z3.
+  logTextLn ">>>> gcl.debug: PATH / z3"
+  mPath <- liftIO (lookupEnv "PATH")
+  logTextLn $ Text.pack $ "PATH=" ++ maybe "(unset)" id mPath
+  mZ3 <- liftIO (findExecutable "z3")
+  logTextLn $ Text.pack $ "findExecutable z3 => " ++ maybe "NOT FOUND" id mZ3
+  logTextLn "<<<< gcl.debug: PATH / z3"
+
   logTextLn ">>>> gcl.debug: FileState"
   maybeFs <- getFileState filePath
   case maybeFs of
